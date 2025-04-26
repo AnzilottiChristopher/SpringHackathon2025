b822ce66cd9f3647976fc43cca67b613c4a8bdee
Chris Anzilotti <anzilottichr@gmail.com>
2025-04-26 10:35:51 -0400

    Added Color Tile logic

--- src/Tile.java
+++ src/Tile.java
@@ -3,14 +3,36 @@ public class Tile
     private boolean dirty;
     private boolean obstacle;
     private boolean enemy;
+    private boolean isPlayer;
+
+
+    //Dirty = brown, Vacuum Sprite = Player, Grey = wall, Dog Sprite = enemy
     private String color;
 
-    public Tile(boolean dirty, boolean obstacle, boolean enemy, String color)
+    public Tile(boolean dirty, boolean obstacle, boolean enemy)
     {
         this.dirty = dirty;
         this.obstacle = obstacle;
         this.enemy = enemy;
-        this.color = color;
+        if (isDirty())
+        {
+            this.color = "brown";
+        }
+        else if (isObstacle())
+        {
+            this.color = "grey";
+        }
+        else if (isPlayer)
+        {
+            this.color = "spritePlayer";
+        }
+        else if (isEnemy())
+        {
+            this.color = "spriteDog";
+        } else
+        {
+            this.color = "white";
+        }
     }
 
     public boolean isDirty()
